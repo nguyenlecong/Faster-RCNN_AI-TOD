1520a171e037ae1e578f1186154f5e1e428bb17b
Le Cong Nguyen <nguyen.lecong@arsvietnam.com>
2023-10-10 17:09:48 +0700

    Refactor code

--- config.py
+++ config.py
@@ -1,25 +1,29 @@
 # Model Config
-min_size = 600  # model 1+4
-# min_size = 1200  # model 2+3
-
+min_size = 600
+max_size = 800
 anchor_ratio = (0.5, 1.0, 2.0)
 
-# anchor_size = (64, 128, 256, 512)  # model 1
-# anchor_size = (32, 64, 128, 256, 512)  # model 2
-anchor_size = (8, 16, 32, 64, 128, 256, 512)  # model 3+4
+anchor_size = (32, 64, 128, 256, 512)  # model 1 - default
+# anchor_size = (8, 16, 32, 64, 128, 256, 512)  # model 2
 
 # Dataset Config
-# class_map = {0: 'airplane', 1: 'ship', 2: 'storage-tank', 3: 'vehicle'}  # model 1+2+3
+
+# XView
+# class_map = {0: 'airplane', 1: 'ship', 2: 'storage-tank', 3: 'vehicle'}
+# train_ratio = 0.7
+# val_ratio = 0.2
+
+# AI-TOD
 class_map = {'airplane': 1, 'bridge': 2 , 'storage-tank': 3, 
              'ship': 4, 'swimming-pool': 5,  'vehicle': 6,
-             'person': 7, 'wind-mill': 8}  # model 4
-train_ratio = 0.7
-val_ratio = 0.2
-train_val_batch = 16
-test_batch = 1
+             'person': 7, 'wind-mill': 8}
+
+train_batch = 8
+val_test_batch = 1
 
 # Train/Test mode
+
 mode = 'train'
+patience = 10  # 1 for debugging
+num_epochs = 10000
 learning_rate = 0.001
-num_epochs = 1000
-patience = 10  # EarlyStopping patience (epochs without improvement)
